070ae2b5ed16850fa960d8005fb7943ec989684c
Ashkan <anasseri@apperian.com>
2012-08-10 18:44:46 -0400

    added register user

--- eVa/evaWebService.m
+++ eVa/evaWebService.m
@@ -31,29 +31,25 @@
     
     [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:self.handler];
     
-    
-    /*NSURLResponse *response;
-    
-    [[NSURLConnection alloc] initWithRequest:request delegate:self.delegate];
-    
-    /*
-    NSError *err;
-    NSData *returnData = [ NSURLConnection sendSynchronousRequest: request returningResponse:&response error:&err];
-    NSString *content = [NSString stringWithUTF8String:[returnData bytes]];
-    NSLog(@"responseData: %@", content);
-
-    NSError *parseError;
-    id parsedObj = [NSJSONSerialization JSONObjectWithData:returnData options:0 error:&parseError];
-    
-    NSDictionary *json_dict = (NSDictionary *)parsedObj;
-    NSLog(@"dict here %@" , json_dict);
-    
-    return json_dict;*/
-    
-    
-    
 }
-                      
+
+- (void) registerUser:(NSString *)email withPassword:(NSString *)password andFirstName:(NSString *)firstName andLastName:(NSString *)lastName{
+    NSString *ghEventsAPIRUL = @"http://192.168.0.106:5000/user/reg";
+    NSURL *url = [[NSURL alloc]initWithString:ghEventsAPIRUL];
+    
+    
+    NSString *myRequestString =  [NSString stringWithFormat:@"email=%@&pw=%@&firstname=%@&lastname=%@", email, password, firstName, lastName];
+    
+    NSData *myRequestData = [ NSData dataWithBytes: [ myRequestString UTF8String ] length: [ myRequestString length ] ];
+    NSMutableURLRequest *request = [ [ NSMutableURLRequest alloc ] initWithURL: url];
+    
+    
+    [ request setHTTPMethod: @"POST" ];
+    [ request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"content-type"];
+    [ request setHTTPBody: myRequestData ];
+    
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:self.handler];
+}
                       
 - (NSString *)urlEncodeValue:(NSString *)str
 {
